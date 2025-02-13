56722e34058818b3eed9b91f24e85d0d0a715755
diego-nst <88235795+diego-nst@users.noreply.github.com>
2025-02-13 23:45:34 +0800

    Add files via upload

---
+++ ledger/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import recipe_list, recipe1, recipe2
+
+urlpatterns = [
+    path('',recipe_list,name='recipe-list'),
+    path('recipes/list', recipe_list, name='recipe-list'),
+    path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2'),
+]
+
+app_name = 'ledger'
