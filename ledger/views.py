22ebc97337f40cd7912aecfaba87dc9c430be414
diego-nst <diegostanalgo1@gmail.com>
2025-02-27 18:07:50 +0800

    implemented models for the recipebook

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Ingredient, Recipe, RecipeIngredient
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'ledger/recipe_list.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe.html'
 
 def recipe_list(request):
     ctx = {
