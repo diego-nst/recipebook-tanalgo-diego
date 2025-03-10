707cd57d206797ddd52fc1d2c22eb408bc7cd117
diego-nst <diegostanalgo1@gmail.com>
2025-03-10 19:52:47 +0800

    created a Profile model in accounts and modified the Recipe model in ledger

--- ledger/models.py
+++ ledger/models.py
@@ -1,27 +1,34 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import Profile
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe', args = [self.pk])
-    
+        return reverse('ledger:recipe', args=[self.pk])
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe', args = [self.pk])
-    
-    
+        return reverse('ledger:recipe', args=[self.pk])
+
+
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL, null=True, related_name='recipe')
-    recipe = models.ForeignKey(Recipe, on_delete=models.SET_NULL, null=True, related_name='ingredients')
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.SET_NULL, null=True, related_name='recipe')
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.SET_NULL, null=True, related_name='ingredients')
