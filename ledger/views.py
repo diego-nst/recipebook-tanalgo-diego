8b3d77beddccd01451edfc48708efc81f53de0a2
diego-nst <diegostanalgo1@gmail.com>
2025-03-10 21:14:35 +0800

    updated detail view to require login

--- ledger/views.py
+++ ledger/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 
@@ -10,9 +11,10 @@ class RecipeListView(ListView):
     template_name = 'ledger/recipe_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
+    redirect_field_name = 'URL'
 
 
 def recipe_list(request):
